08b570ecef6ecb89520b46b1aa189466f20e6a5f
Ankit Pal <ankitpal2441@gmail.com>
2024-11-22 19:21:28 +0530

    Update Random Number Challenge .c

--- Random Number Challenge .c
+++ Random Number Challenge .c
@@ -4,18 +4,13 @@
 
 int main() {
     int RN, UserGuess, attempts = 0;
-
-   
     srand(time(0));
-
-    
     RN = (rand() % 100) + 1;
 
     printf("Welcome to the Number Guessing Game!\n");
     printf("I have chosen a number between 1 and 100.\n");
     printf("Can you guess what it is?\n");
 
-    
     do {
         printf("Enter your guess: ");
         scanf("%d", &UserGuess);
